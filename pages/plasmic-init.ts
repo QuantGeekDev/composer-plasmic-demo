ab4c276c916de46a5e6a8f169a5c4700681867e3
Alex Andru <alex007d@gmail.com>
2024-12-21 21:24:51 +0100

    feat: add plasmic stuff

---
+++ pages/plasmic-init.ts
@@ -0,0 +1,12 @@
+import { initPlasmicLoader } from "@plasmicapp/loader-nextjs";
+export const PLASMIC = initPlasmicLoader({
+  projects: [
+    {
+      id: "nXve4wYifXcofuHGMpuNw8",  // ID of a project you are using
+      token: "0EyReaw1KZR2TvcajfY7xdzB6SH4obdch2xPABVY66dXmE1UqawfbmeiFCCe3T8T139XjSNSwFKJ1yGS4dA"  // API token for that project
+    }
+  ],
+  // Fetches the latest revisions, whether or not they were unpublished!
+  // Disable for production to ensure you render only published changes.
+  preview: true,
+})
